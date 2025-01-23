aad3903b6debeef75946a49edf147b26549b9678
DayiTecnologia <cosy890720@gmail.com>
2025-01-23 10:30:54 -0500

    crud Persona

--- cursodayi_app/urls.py
+++ cursodayi_app/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path('add', views.add_person, name= 'add_person'),
+    path('delete/<int:person_id>/', views.delete_person, name= 'delete_person'),
+    path('update/<int:person_id>/', views.update_person, name= 'update_person'),
+
+
+
 ]
--- cursodayi_app/views.py
+++ cursodayi_app/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,redirect
+from .forms import PersonForm
+from .models import Person
+
 
 # Create your views here.
 def index(request):
-    return render (request, 'index.html')
+    persons = Person.objects.all()
+    print(persons)
+    return render (request, 'index.html', {'persons':persons})
+
+def add_person(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = PersonForm()
+    return render(request, 'index.html', {'form': form})
+
+def delete_person(request, person_id):
+    person = get_object_or_404(Person, id=person_id)
+    if request.method == 'POST':
+        person.delete()
+        return redirect('index')
+    return render(request, 'index.html', {'person': person} )
+
+def update_person(request, person_id):
+    person = get_object_or_404(Person, id=person_id)
+    if request.method == 'POST':
+        form = PersonForm(request.POST, instance=person)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = PersonForm(instance=person)
+    return render(request, 'update_person.html', {'form': form})
+
